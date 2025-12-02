d760726bbbf070b25d26d5d226d392fd6d147ae1
jacob whitty <jacobwhitty6@gmail.com>
2025-12-02 14:31:12 +1300

    moved config to env

--- carbon-poc/config/firebase.tsx
+++ carbon-poc/config/firebase.tsx
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore} from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD4uMw8rfSn4m0Tu6c4Yq5G9pOoR0yhHX0",
-  authDomain: "carbon-app-2006b.firebaseapp.com",
-  projectId: "carbon-app-2006b",
-  storageBucket: "carbon-app-2006b.firebasestorage.app",
-  messagingSenderId: "956630781092",
-  appId: "1:956630781092:web:ba299db972309e6e4b6a31",
-  measurementId: "G-5EF3DQ9XCC"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
