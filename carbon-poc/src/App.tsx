223e0d3e1716900fef7918b3efa317b1216431fc
jacob whitty <jacobwhitty6@gmail.com>
2025-12-02 14:40:22 +1300

    fixed merge conflict

--- carbon-poc/src/App.tsx
+++ carbon-poc/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import { ImageUpload } from './components/ImageUpload';
 import { Dashboard } from './components/dashBoard';
 import { Login } from './auth/login';
